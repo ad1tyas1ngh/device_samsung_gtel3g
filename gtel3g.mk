0adb816d4d77f7040c02b839670b0e7a5ab435ce
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-03-28 17:31:20 +0530

    gtel3g: unify with scx35-common

--- gtel3g.mk
+++ gtel3g.mk
@@ -20,11 +20,98 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Inherit from vendor tree
 $(call inherit-product-if-exists, vendor/samsung/gtel3g/gtel3g-vendor.mk)
 
-# Inherit from scx35-common device configuration
-$(call inherit-product, device/samsung/scx35-common/common.mk)
+# Inherit from AOSP product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from sprd-common device configuration
+$(call inherit-product, device/samsung/sprd-common/common.mk)
 
 # Inherit scx30g-common vendor tree
 $(call inherit-product-if-exists, vendor/samsung/scx30g-common/scx30g-common-vendor.mk)
+
+# Audio
+PRODUCT_PACKAGES += \
+	audio_hw.xml \
+	audio_para \
+	audio_effects_vendor.conf \
+	audio_policy.conf \
+	codec_pga.xml \
+	tiny_hw.xml \
+	audio.primary.sc8830 \
+	libaudio-resampler
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+	libbluetooth_jni \
+	bluetooth.default
+
+# Codecs
+PRODUCT_PACKAGES += \
+	libcolorformat_switcher \
+	libstagefrighthw \
+	libstagefright_sprd_mpeg4dec \
+	libstagefright_sprd_mpeg4enc \
+	libstagefright_sprd_h264dec \
+	libstagefright_sprd_h264enc \
+	libstagefright_sprd_vpxdec \
+	libstagefright_sprd_aacdec \
+	libstagefright_sprd_mp3dec
+
+# Media config
+PRODUCT_PACKAGES += \
+	media_codecs.xml
+
+MEDIA_XML_CONFIGS := \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(MEDIA_XML_CONFIGS),$(f):system/etc/$(notdir $(f)))
+
+# Common libs
+PRODUCT_PACKAGES += \
+	libstlport \
+	librilutils \
+	libril_shim \
+	libgps_shim
+
+# GPS
+PRODUCT_PACKAGES += \
+	libgpspc \
+	libefuse \
+	gps.conf \
+	gps.xml
+
+# HWC
+PRODUCT_PACKAGES += \
+	libHWCUtils \
+	gralloc.sc8830 \
+	libmemoryheapion \
+	libion_sprd
+
+# System init.rc files
+PRODUCT_PACKAGES += \
+	at_distributor.rc \
+	chown_service.rc \
+	data.rc \
+	dns.rc \
+	engpc.rc \
+	gpsd.rc \
+	hostapd.rc \
+	kill_phone.rc \
+	macloader.rc \
+	mediacodec.rc \
+	modem_control.rc \
+	modemd.rc \
+	nvitemd.rc \
+	p2p_supplicant.rc \
+	phoneserver.rc \
+	refnotify.rc \
+	set_mac.rc \
+	smd_symlink.rc \
+	swap.rc \
+	wpa_supplicant.rc
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -53,7 +140,9 @@ PRODUCT_PACKAGES += \
 	init.sc8830.usb.rc \
 	init.gtel3g_base.rc \
 	ueventd.sc8830.rc \
-        fstab.sc8830
+        fstab.sc8830 \
+        init.board.rc \
+	init.wifi.rc
 
 # RIL
 PRODUCT_PACKAGES += \
@@ -63,8 +152,83 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sys.sdcardfs=true
 
+# Packages
+PRODUCT_PACKAGES += \
+	SamsungDoze \
+	Jelly \
+	Snap
+
+# Lights
+PRODUCT_PACKAGES += \
+	lights.sc8830
+
+# PowerHAL
+PRODUCT_PACKAGES += \
+	power.sc8830
+
+# Camera config
+PRODUCT_PROPERTY_OVERRIDES += \
+	camera.disable_zsl_mode=1
+
+# Languages
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.product.locale.language=en \
+	ro.product.locale.region=GB
+
 # WiFi
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+
+# WiFi
+PRODUCT_PACKAGES += \
+	macloader \
+	wpa_supplicant.conf \
+	wpa_supplicant_overlay.conf \
+	p2p_supplicant_overlay.conf
+
+# Disable mobile data on first boot
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.android.mobiledata=false
+
+# ART device props
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dex2oat-flags=--no-watch-dog
+
+# Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+	sys.use_fifo_ui=1
+
+# Permissions
+PERMISSIONS_XML_FILES := \
+	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.sensor.proximity.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.software.midi.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml
+
+PRODUCT_COPY_FILES += \
+	$(foreach f,$(PERMISSIONS_XML_FILES),$(f):system/etc/permissions/$(notdir $(f)))
+
+# enable Google-specific location features,
+# like NetworkLocationProvider and LocationCollector
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.com.google.locationfeatures=1 \
+	ro.com.google.networklocation=1
+
+# Dalvik heap config
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+
+
+# For userdebug builds
+ADDITIONAL_DEFAULT_PROPERTIES += \
+	ro.secure=0 \
+	ro.adb.secure=0 \
+	ro.debuggable=1 \
+	persist.sys.root_access=1 \
+	persist.service.adb.enable=1
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_gtel3g
