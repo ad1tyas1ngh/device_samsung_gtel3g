129c924c26b0650f13a5fb05cd6af80267b7fbda
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-04-16 15:03:34 +0530

    gtel3g: add/update LineageOS copyright headers

--- device.mk
+++ device.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2017 The Lineage Project
+#
+# Copyright (C) 2021 The Lineage Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
